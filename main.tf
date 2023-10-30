f67a9d0745f0c6ea7f1cfdcd6fcbeca56c66525e
prasannaballa <128133786+prasannaballa@users.noreply.github.com>
2023-10-30 19:54:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,14 +28,12 @@ terraform {
   }
 }
 
+
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
-  username               = azurerm_kubernetes_cluster.main.kube_config.0.username
-  password               = azurerm_kubernetes_cluster.main.kube_config.0.password
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
-}
+config_path    = "home/prasanna/.kube/config"
+  config_context = "sivaaks"
+  }
+
 # Use data block to fetch the AKS cluster credentials
 data "azurerm_kubernetes_cluster" "existing_aks" {
   name                = "sivaaks"
